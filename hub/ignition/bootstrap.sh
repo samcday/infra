d7e63fbeb439a457541c5132a83ddfcecaf051c5
Sam Day <me@samcday.com>
2025-07-31 15:55:58 +1000

    hub/ignition: initial import
    adapted from samcday/home-cluster ignition configs.
    
    includes a bootstrap.sh script that can be used to generate some
    Ignition to bring up the initial nodes (i.e before the cluster is alive
    and able to handle PXE provisioning itself)
    
    The etcd config starts up etcd members that use SRV discovery mechanism
    and TLS encryption+authentication.
    
    Signed-off-by: Sam Day <me@samcday.com>

---
+++ hub/ignition/bootstrap.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -uexo pipefail
+
+# This should only be used when petting the first node of a new cluster.
+workdir=$(mktemp -d)
+
+hostname="${hostname:-hub-az1-cp1}"
+
+configmerge=
+
+profiles="base var"
+
+if [[ "${TPM2:-1}" -eq "1" ]]; then
+  profiles="$profiles tpm"
+fi
+
+for f in $profiles; do
+  sops -d --input-type=yaml --output-type=yaml "$f.bu" | podman run -v "$(pwd)/files":/files --rm -i quay.io/coreos/butane:release -d /files > "$workdir/$f.ign"
+  configmerge="$configmerge
+    - local: $f.ign"
+done
+
+podman run -v "$workdir":/files --rm -i quay.io/coreos/butane:release -d /files <<HERE
+variant: fcos
+version: 1.5.0
+ignition:
+  config:
+    merge: $configmerge
+storage:
+  files:
+    - path: /etc/hostname
+      mode: 0644
+      contents:
+        inline: $hostname
+HERE
