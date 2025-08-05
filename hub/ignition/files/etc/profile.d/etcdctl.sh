79d786df211efd673f801d52c38b51e13d8d07be
Sam Day <me@samcday.com>
2025-08-05 14:34:19 +1000

    hub/ignition: add etcdctl alias

--- hub/ignition/files/etc/profile.d/etcdctl.sh
+++ hub/ignition/files/etc/profile.d/etcdctl.sh
@@ -2,3 +2,5 @@ export ETCDCTL_KEY=/var/lib/etcd/peer-key.pem
 export ETCDCTL_CERT=/var/lib/etcd/peer.pem
 export ETCDCTL_CACERT=/var/lib/etcd/ca.pem
 export ETCDCTL_DISCOVERY_SRV=etcd.hub.internal
+
+alias etcdctl='sudo -E podman exec -it --env=ETCDCTL_* etcd etcdctl'
