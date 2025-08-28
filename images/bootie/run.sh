22b553f3092d5b9a1ff937df4c4284d759631373
Sam Day <me@samcday.com>
2025-08-28 16:03:47 +1000

    images/bootie: compile Butane on startup
    Signed-off-by: Sam Day <me@samcday.com>

--- images/bootie/run.sh
+++ images/bootie/run.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
+
+set -uexo pipefail
+
+if [[ -d /butane ]]; then
+    for f in /butane/*.yaml; do
+        name=$(basename "$f")
+        butane --strict < "$f" > "/ignition/${name/.yaml}".ign
+    done
+fi
+
 fcgiwrap -s 'tcp:0.0.0.0:9000' &
 nginx
