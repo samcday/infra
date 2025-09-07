a4c842d99054e6ea9cf56b27cf64c536e3815610
Sam Day <me@samcday.com>
2025-09-07 19:43:04 +1000

    hub/tofu: expand cloud-cluster cloudflare API token privs
    ... so basically, back to where it was when I first copy/pasted it.

--- hub/tofu/cloudflare.tf
+++ hub/tofu/cloudflare.tf
@@ -62,15 +62,30 @@ resource "kubernetes_secret" "cert-manager-token" {
   }
 }
 
-# cloud-cluster has its own external-dns instance that can reuse the main token
-# that hub's external-dns uses
-resource "kubernetes_secret" "cloud-cluster-external-dns" {
+# cloud-cluster writes DNS records (external-dns) and manages cloudflared tunnels
+resource "cloudflare_api_token" "cloud-cluster" {
+  name = "hub-cloud-cluster"
+
+  policy {
+    permission_groups = [
+      data.cloudflare_api_token_permission_groups.all.account["Cloudflare Tunnel Write"],
+      data.cloudflare_api_token_permission_groups.all.zone["DNS Write"],
+    ]
+    resources = {
+      "com.cloudflare.api.account.*" = "*"
+      "com.cloudflare.api.account.zone.${data.cloudflare_zone.samcday.id}" = "*"
+    }
+  }
+}
+
+
+resource "kubernetes_secret" "cloud-cluster-cloud-cluster" {
   metadata {
     name      = "cloudflare"
     namespace = "cloud-cluster"
   }
 
   data = {
-    "token" = cloudflare_api_token.external-dns.value
+    "token" = cloudflare_api_token.cloud-cluster.value
   }
 }
