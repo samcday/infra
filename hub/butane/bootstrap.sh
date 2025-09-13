6e9278770503a52326783aff3d995f7585fd0193
Sam Day <me@samcday.com>
2025-09-13 16:09:45 +1000

    treewide: DRY up Butane configs
    Introduce common/butane and move a bunch of stuff here.
    
    Butane's config merging is used to pull these common bits in where
    needed.
    
    Update bootie deployments and hub's bootstrap.sh to compile the
    common configs and make those available for the config merging.
    
    For now the configs in common/butane are not encrypted since I couldn't
    think of any reasons they should be. Maybe that'll change in future.

--- hub/butane/bootstrap.sh
+++ hub/butane/bootstrap.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 set -uexo pipefail
 
+cd "$(dirname "$0")"
+
 # This should only be used when petting the first node of a new cluster.
 workdir=$(mktemp -d)
 
 configmerge=
 
-profiles="base control-plane etcd install var"
+profiles="base control-plane etcd install"
 
-if [[ "${TPM2:-1}" -eq "1" ]]; then
-  profiles="$profiles tpm"
-fi
+mkdir -p $workdir/common
+for f in ../../common/butane/*.yaml; do
+  name=$(basename $f)
+  [[ "$name" == "kustomization.yaml" ]] && continue
+  podman run --rm -i quay.io/coreos/butane:release > "$workdir/common/${name//.yaml}.ign" < $f
+done
 
 for f in $profiles; do
-  sops -d "$f.yaml" | podman run --rm -i quay.io/coreos/butane:release > "$workdir/$f.ign"
+  sops -d "$f.yaml" | podman run -v "$workdir":/files --rm -i quay.io/coreos/butane:release -d /files > "$workdir/$f.ign"
   configmerge="$configmerge
     - local: $f.ign"
 done
